cce34213b67338b5924b3d8907820cf51647d2a6
Shun Iwashita <iwashunabc519@gmail.com>
2023-10-16 16:15:31 +0700

    updae

--- module/mapped-types.ts
+++ module/mapped-types.ts
@@ -1,7 +1,3 @@
-type Methods = {
-  [key: string]: (...params: any[]) => any;
-}
-
 type choice = "yes" | "no";
 type SomeArgsOnlyBool = {
   [key: string]: boolean | choice
@@ -11,17 +7,33 @@ const check: SomeArgsOnlyBool = {
   ready: 'no',
   connected: false
 }
+check.strict = "yes";
 
-const httpRequest: Methods = {
-  get: (url: string) => {
+type Methods = {
+  [key: string]: (...params: any[]) => any;
+}
+type XHRMethods = {
+  get(url: string): { status: number, data: JSON | Object | string },
+  post<T extends Object>(url: string, data: T, token: { isExpired: boolean, csrf_token: string }): { status: number, data: T } | { status: number, error: Error }
+}
+
+const myRequest: XHRMethods & Methods = {
+  get: (url) => {
     console.log("fetch", url);
     const data = [1,2,3].toString();
     return JSON.parse(data);
   },
-  post: (url: string, data: Object, token: { isExpired: boolean, csrf_token: string }) => {
+  post: (url, data, token) => {
     if(token.isExpired) return { status: 400, error: new Error("Token Expired") }
     console.log(`Sending data ${JSON.stringify(data)} to ${url}`);
     // post something
     return { status: 200, data };
+  },
+  getUser: (url, username: string) => {
+    console.log(`getting user ${username}'s data from ${url}`);
+    return {
+      status: 'online',
+      profile: { name: 'Frank', email: 'frank@gmail.com' }
+    }
   }
 }
